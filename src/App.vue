98c7078ee02362db187656c0997ec7e8f1bd0112
PedrooLska <pedrolucas81@hotmail.com>
2021-06-21 12:44:36 -0300

    Add responsive design

--- src/App.vue
+++ src/App.vue
@@ -67,7 +67,7 @@ body {
 
     &__column {
       display: grid;
-      grid-template-columns: repeat(3, 1fr);
+      grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
       grid-gap: 20px;
       margin-top: 30px;
     }
