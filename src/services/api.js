6f46ae600168f31a1bdacc206545885075382a37
PedrooLska <pedrolucas81@hotmail.com>
2021-06-21 12:38:50 -0300

    Correction Headers Request

--- src/services/api.js
+++ src/services/api.js
@@ -3,9 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://me-frontend-challenge-api.herokuapp.com/",
   headers: {
-    "User-Agent": {
-      "Content-Type": "application/json",
-    },
+    "Content-Type": "application/json",
   },
 });
 
